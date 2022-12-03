9a7a31915694be882551d90e4b8fe7a9a1f266d6
rg3915 <regis42santos@gmail.com>
2022-12-03 03:31:47 -0300

    Pegando dados da rota atual

--- my-app/src/views/Team.vue
+++ my-app/src/views/Team.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h1>Equipe</h1>
+    <h2>{{ member }}</h2>
+
+    <div v-if="member === 'regis'">
+      Bio Regis
+    </div>
+
+    <div v-else-if="member === 'tiago'">
+      Bio Tiago
+    </div>
+
+    <div v-else>
+      Membro comum
+    </div>
+
     <p>Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mim que vai caçá sua turmis! Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Quem num gosta di mé, boa gentis num é. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.</p>
   </div>
 </template>
@@ -9,8 +23,11 @@
   export default {
     name: 'Team',
     data() {
-      return {
-        
+      return {}
+    },
+    computed: {
+      member() {
+        return this.$route.params.member
       }
     },
     methods: {}
